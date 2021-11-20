b9d01b6679ff77b089193179a303765b5de11ede
brayangomez08 <gomezmancobrayanalexander@gmail.com>
2021-11-20 17:21:22 -0500

    The server and database configuration was added

---
+++ src/app.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const morgan = require('morgan');
+const multer = require('multer');
+const path = require('path');
+const { engine } = require('express-handlebars');
+
+// Initializations
+const app = express();
+require('./database');
+
+// Settings
+app.set('port', 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine(
+	'.hbs',
+	engine({
+		defaultLayout: 'main',
+		layoutsDir: path.join(app.get('views'), 'layouts'),
+		partialsDir: path.join(app.get('views'), 'partials'),
+		extname: '.hbs',
+	})
+);
+app.set('view engine', '.hbs');
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const storage = multer.diskStorage({
+	destination: path.join(__dirname, 'public/uploads'),
+	filename: (req, file, cb) => {
+		cb(null, new Date().getTime() + path.extname(file.originalname));
+	},
+});
+
+app.use(multer({ storage }).single('image'));
+
+// Routes
+app.use(require('./routes'));
+
+module.exports = app;
